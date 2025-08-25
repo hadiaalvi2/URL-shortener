6d640aeea86d13b07806d0e5e0176f2573013c54
hadiaalvi2 <hadia.alvi@zimogroup.org>
2025-08-25 09:03:35 +0500

    updated

--- url-shortener/src/app/[shortCode]/page.tsx
+++ url-shortener/src/app/[shortCode]/page.tsx
@@ -10,6 +10,7 @@ const baseUrl = process.env.VERCEL_URL
   ? `https://${process.env.VERCEL_URL}`
   : process.env.NEXT_PUBLIC_BASE_URL || "http://localhost:3001";
 
+
 interface FetchedMetadata {
   title: string;
   description: string;
