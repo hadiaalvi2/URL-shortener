efc4d61778f747f2781ae1a2d93280d25ae8877c
hadiaalvi2 <hadia.alvi@zimogroup.org>
2025-08-26 19:53:54 +0500

    METADATA FIXING

--- url-shortener/src/app/api/og/route.ts
+++ url-shortener/src/app/api/og/route.ts
@@ -32,7 +32,12 @@ export async function GET(req: Request) {
         'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
         'Accept-Language': 'en-US,en;q=0.5',
         'Connection': 'keep-alive',
-        'Upgrade-Insecure-Requests': '1'
+        'Upgrade-Insecure-Requests': '1',
+        'Referer': parsedUrl.origin, // Add Referer header
+        'Sec-Fetch-Dest': 'document',
+        'Sec-Fetch-Mode': 'navigate',
+        'Sec-Fetch-Site': 'none',
+        'Sec-Fetch-User': '?1',
       }
     });
 
